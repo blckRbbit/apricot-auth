f38c9946f92193de451980fcda28784e1094cc44
blckRbbit <lex.leonidov@gmail.com>
2024-06-15 19:19:11 +0300

    initial

---
+++ src/main/java/ru/abolsoft/apricot/auth/ApricotAuthApplication.java
@@ -0,0 +1,14 @@
+package ru.abolsoft.apricot.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://www.youtube.com/watch?v=iURuqbGeRNc&list=PLs_aLxm3VDLu9ghcQhTHOT84zmqLgAwdL&index=11 25:50
+@SpringBootApplication
+public class ApricotAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApricotAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/abolsoft/apricot/auth/ApricotAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abolsoft.apricot.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApricotAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
